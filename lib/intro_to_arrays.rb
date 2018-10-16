9925fe01fc956510a2aa4c4f29612aa2ec2ac22c
Meghan Campbell <mlc03e@gmail.com>
2018-10-16 20:59:42 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,20 @@
+def instantiate_new_array 
+ Array.new
+end
+
+def array_with_two_elements
+  Array.new(2)
+end
+
+def first_element(taylor_swift)
+   taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[-1]
+end
+
