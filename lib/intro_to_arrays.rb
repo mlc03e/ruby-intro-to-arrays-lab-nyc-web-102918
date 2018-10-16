122a032e1072a6c3fd2b8b7ad3e618530cfcabd6
Meghan Campbell <mlc03e@gmail.com>
2018-10-16 21:06:23 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array 
+ Array.new
+end
+
+def array_with_two_elements
+  Array.new(2)
+end
+
+def first_element(taylor_swift)
+   taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  first_country.first 
+end
+
+def last_element_with_array_methods(last_country)
+  last_country.last 
+end
+
+def length_of_array(length)
+  length.length
+end
